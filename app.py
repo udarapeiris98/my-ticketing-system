504f4491c95c47fefb08b69a17b7bbbec6423708
udarapeiris98 <udarapeiris98@gmail.com>
2026-04-28 10:37:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ st.set_page_config(
 )
 
 # නිවැරදි ක්‍රමය:
+# Me vidihata thiyenna oni:
 SUPABASE_URL = st.secrets["https://emqwhmimrajxhioiumab.supabase.co/rest/v1/"]
 SUPABASE_KEY = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtcXdobWltcmFqeGhpb2l1bWFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNDY5NjYsImV4cCI6MjA5MjkyMjk2Nn0.hoHCuJCk7lwHUMjn4dnJg5Wyj_gZ5tb0aWt5glmGbUk"]
 @st.cache_resource
